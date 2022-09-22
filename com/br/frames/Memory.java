abec645cd220cc024f4b8a7f7d8477d20ad5e32d
MuriloApa <100038314+MuriloApa@users.noreply.github.com>
2022-09-22 13:57:34 -0300

    Update Memory.java

--- com/br/frames/Memory.java
+++ com/br/frames/Memory.java
@@ -25,40 +25,6 @@ public class Memory {
 
         for (int i = 0; i < frames.length; i++) {
             if (frames[i] != null) {
-                relatorio += i + "\t" + frames[i].getNumber() + "\t";
-                if (frames[i].getPage() == null) {
-                    relatorio += "----------\n";
-                } else {
-                    relatorio += frames[i].getPage().toString() + "\n";
-                }
-            } else {
-package com.br.frames;
-
-/**
- *
- * @author murilo
- */
-public class Memory {
-    private Frame[] frames;
-
-    public Frame[] getMemory() {
-        return frames;
-    }
-
-    public void setMemory(Frame[] memory) {
-        this.frames = memory;
-    }
-
-    public Memory(int numFrames) {
-        this.frames = new Frame[numFrames];
-    }
-
-    @Override
-    public String toString() {
-        String relatorio = "Frame\tPágina\tConteúdo\n";
-
-        for (int i = 0; i < frames.length; i++) {
-            if (frames[i] != null) {
                 
                 if (frames[i].getPage() == null) {
                     relatorio += i + "\t-" + "\t----------\n";
@@ -69,11 +35,6 @@ public class Memory {
             } else {
                 relatorio += i + "\t-" + "\t----------\n";
             }
-        }
-        return relatorio;
-    }
-
-}           
         }
         return relatorio;
     }
