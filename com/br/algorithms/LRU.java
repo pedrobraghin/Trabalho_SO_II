18ba2b4175d973859f4d6f0002fc7d9a406930cb
MuriloApa <100038314+MuriloApa@users.noreply.github.com>
2022-09-25 16:51:14 -0600

    Update LRU.java
    Adicionando comentários no código

--- com/br/algorithms/LRU.java
+++ com/br/algorithms/LRU.java
@@ -102,12 +102,20 @@ public class LRU extends Algorithm {
         return report;
     }
 
+    /**
+     * Método para desenfileirar o próximo frame de memória disponível
+     * @return
+     */
     public int dequeueMemory() {
         int pos = this.inicioFilaFrames;
         this.inicioFilaFrames++;
         return pos;
     }
 
+    /**
+     * Método para desempilhar a pilha de frames ocupados
+     * @return
+     */
     public int unstackFrame() {
         int pos = -1;
 
@@ -118,6 +126,10 @@ public class LRU extends Algorithm {
         return pos;
     }
 
+    /*
+     * Método para empilhar um frame na pilha de frames ocupados
+     * @param x
+    */
     public void stackFrame(int x) {
 
         if (this.topo_frames != this.numFrames - 1) {
@@ -127,6 +139,10 @@ public class LRU extends Algorithm {
 
     }
 
+    /**
+     * Método para desempilhar a pilha auxiliar de frames ocupados
+     * @return
+     */
     public int unstackAux() {
         int pos = -1;
 
@@ -138,6 +154,10 @@ public class LRU extends Algorithm {
         return pos;
     }
 
+    /*
+     * Método para empilhar um frame na pilha auxiliar de frames ocupados
+     * @param x
+    */
     public void stackAux(int x) {
 
         if (this.topoAux != this.numFrames - 1) {
@@ -147,11 +167,22 @@ public class LRU extends Algorithm {
 
     }
 
+    /**
+     * Método para atualizar uma página que um frame guarda
+     * @param nFrame
+     * @param numPag
+     * @param pagina
+     */
     public void updateFrame(int nFrame, int numPag, Page pagina) {
         memory.getMemory()[nFrame].setNumber(numPag);
         memory.getMemory()[nFrame].setPage(pagina);
     }
 
+    /**
+     * Método para adicionar uma nova página a um frame desocupado
+     * @param numPage
+     * @param pagina
+     */
     public void addPage(int numPage, Page pagina) {
         int nFrame = dequeueMemory();
 
@@ -159,6 +190,10 @@ public class LRU extends Algorithm {
         updateFrame(nFrame, numPage, pagina);
     }
 
+    /**
+     * Método para atualizar a pilha de frames ocupados, o frame recém acessado vai para o topo da fila
+     * @param x
+     */
     public void updateStacks(int x) {
 
         int temp = unstackFrame();
@@ -175,6 +210,12 @@ public class LRU extends Algorithm {
         stackFrame(temp);
     }
 
+    /**
+     * Método responsável por trocar a página de um frame. O frame que terá sua página trocada será aquele que estiver no 
+     * inicio da pilha de frames ocupados, ou seja, o que não foi acessado a mais tempo.
+     * @param numPage
+     * @param page
+     */
     public void changePage(int numPage, Page page) {
         while (topo_frames != 0) {
             stackAux(unstackFrame());
@@ -187,6 +228,9 @@ public class LRU extends Algorithm {
         updateFrame(nFrame, numPage, page);
     }
 
+    /*
+     * Método para gerar a sequência aleatória de páginas requeridas
+     */
     public void generatePageRequest() {
         Random random = new Random();
         for (int i = 0; i < this.numPagReq; i++) {
@@ -195,6 +239,12 @@ public class LRU extends Algorithm {
         this.isRunning = true;
     }
 
+    /**
+     * Busca a página passada como argumento na pilha de frames ocupados.
+     * Se a página estiver alocada em algum frame, é retornada a posição desse frame na memória, senão, -1.
+     * @param pagina
+     * @return
+     */
     public int searchPage(int pagina) {
         int frame = -1;
         if (topo_frames == -1) {
@@ -208,6 +258,9 @@ public class LRU extends Algorithm {
         return frame;
     }
 
+    /*
+     * Instância um frame para cada posição da memória, um array de frames.
+     */
     public void loadMemory() {
         for (int i = 0; i < numFrames; i++) {
             memory.getMemory()[i] = new Frame();
